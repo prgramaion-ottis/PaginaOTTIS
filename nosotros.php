9b58e6928d311814803c8c8e579a57cdae436cce
programaion-ottis <programacion.ottis@gmail.com>
2022-12-01 12:57:24 -0500

    Inicio Nosotros

---
+++ nosotros.php
@@ -0,0 +1,17 @@
+
+    <?php
+    require("templates/cabecera.html");
+    ?>
+    <body>
+        <div class="container">
+            <?php
+                require("templates/navbar.html");
+            ?>
+            <div class="col-12">
+                <img src="./imagenes/nosotros/Mesa_de_trabajo_9.png" alt="" width="100%">                
+            </div>
+
+            
+
+        </div>
+    
