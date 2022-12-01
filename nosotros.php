eebcb13887645ae5b1bf4ce184f68104636ff0d5
programaion-ottis <programacion.ottis@gmail.com>
2022-12-01 18:39:32 -0500

    Descargar Bootstrap 5.2.3

--- nosotros.php
+++ nosotros.php
@@ -16,7 +16,42 @@
         <div class="col-12 col-xs-12 col-sm-12 col-md-6 offset-1 titulo">
             <h1 class="wellcome">¿QUIÉNES SOMOS?</h1>
         </div>
+        <div class="nosotros">
+            <div class="text-1 col-12">
+
+            </div>
+        </div>
+
+    <nav class="navbar">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup"  aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="empresa.php">Empresa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="Informacion-de-usuarios">Información de usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="Comunicaciones">Comuniciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="Mide-tu-velocidad">Mide tu velocidad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-item nav-link " href="contacto.php">Contactanos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>    
+    
 
 
     </div>
+    
     
