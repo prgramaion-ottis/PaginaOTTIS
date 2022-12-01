e55b2d320c61888aa18a701d591cff33c246866c
programaion-ottis <programacion.ottis@gmail.com>
2022-12-01 12:18:13 -0500

    Front contacto terminado

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function over_name(){
+    div_name=document.getElementById("name");
+    console.log("hola");
+    
+}
+function out_name(){
+    div_name=document.getElementById("name");
+    console.log("hola out");
+}
