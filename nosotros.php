546863f8c7c4be35a186887e17259559fc1334e7
programaion-ottis <programacion.ottis@gmail.com>
2022-12-05 08:21:50 -0500

    Nombre imagen

--- nosotros.php
+++ nosotros.php
@@ -4,7 +4,7 @@
 ?>
 <body>
 <script src="js/script.js"></script>
-<link rel="stylesheet" href="css/stylecontact.css" crossorigin="anonymous">
+<link rel="stylesheet" href="css/stylenosotros.css" crossorigin="anonymous">
     <div class="container">
         <?php
             require("templates/navbar.html");
@@ -12,18 +12,35 @@
         <div class="col-12">
             <img src="./imagenes/nosotros/Mesa_de_trabajo_9.png" alt="" width="100%">                
         </div>
-        <br>
         <div class="col-12 col-xs-12 col-sm-12 col-md-6 offset-1 titulo">
             <h1 class="wellcome">¿QUIÉNES SOMOS?</h1>
         </div>
         <div class="nosotros">
-            <div class="text-1 col-12">
-
+            <div class="row row-cols-2 justify-content-end">
+                <div class="vehiculos">
+                    <img src="./imagenes/nosotros/Mesa_de_trabajo_70.png" height="500px" width="600px" alt="">
+                </div>
+                <div class="offset-1 col-10 col-sm-10 col-md-4">
+                    <p class="text-1"><b>Somos un PRST</b> boyacense líder en el mercado, que busca suplir con calidad y competitividad las necesidades de nuestros clientes y apoyar el
+                            crecimiento tecnológico de la región, integrando soluciones propias, individuales, escalables al igual que herramientas (privativas y de 
+                            open source) bajó el desarrollo integral de nuestro recurso humano. Nuestra empresa se rige y está ceñida a las normas vigentes de la 
+                            legislación y regulación de Colombia, bajo la resolución No 29343 que confiere el registro de signo distintivo OTTIS HARDWARE y OTTIS SOFTWARE,
+                            con lo cual se garantiza la calidad de nuestros productos y servicios en busca de la fidelización de nuestros clientes.
+                        
+                    </p>
+                </div>
+                
             </div>
+        </div>
+        <div class=""  >
+            <img src="./imagenes/nosotros/Mesa_de_trabajo_5.png" alt=""  height="100%" width="100%">
+            <!-- <div class="img-cracks col-11 col-md-9 offset-1">
+                <img src="./imagenes/nosotros/Mesa_de_trabajo_2.png" alt=""  height="100%">
+            </div> -->
         </div>
 
      
-    
+    <br>
 
 
     </div>
