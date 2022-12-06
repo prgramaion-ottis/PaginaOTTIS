7e33d8028a40a75aa2e462bf7e5d02d1e906b240
programaion-ottis <programacion.ottis@gmail.com>
2022-12-06 10:59:35 -0500

    nosotro mision

--- nosotros.php
+++ nosotros.php
@@ -17,26 +17,43 @@
         </div>
         <div class="nosotros">
             <div class="row row-cols-2 justify-content-end">
-                <div class="vehiculos">
-                    <img src="./imagenes/nosotros/Mesa_de_trabajo_70.png" height="500px" width="600px" alt="">
+                <div class="vehiculos d-none d-sm-none d-md-none d-lg-block">
+                    <img src="./imagenes/nosotros/Mesa_de_trabajo_70.png" style="z-index:5;" height="500px" width="600px" alt="">
                 </div>
-                <div class="offset-1 col-10 col-sm-10 col-md-4">
+                <div class="offset-1 col-10 col-sm-10 col-md-11 col-lg-4">
                     <p class="text-1"><b>Somos un PRST</b> boyacense líder en el mercado, que busca suplir con calidad y competitividad las necesidades de nuestros clientes y apoyar el
                             crecimiento tecnológico de la región, integrando soluciones propias, individuales, escalables al igual que herramientas (privativas y de 
                             open source) bajó el desarrollo integral de nuestro recurso humano. Nuestra empresa se rige y está ceñida a las normas vigentes de la 
                             legislación y regulación de Colombia, bajo la resolución No 29343 que confiere el registro de signo distintivo OTTIS HARDWARE y OTTIS SOFTWARE,
-                            con lo cual se garantiza la calidad de nuestros productos y servicios en busca de la fidelización de nuestros clientes.
-                        
+                            con lo cual se garantiza la calidad de nuestros productos y servicios en busca de la fidelización de nuestros clientes.    
                     </p>
                 </div>
                 
             </div>
         </div>
-        <div class=""  >
-            <img src="./imagenes/nosotros/Mesa_de_trabajo_5.png" alt=""  height="100%" width="100%">
-            <!-- <div class="img-cracks col-11 col-md-9 offset-1">
-                <img src="./imagenes/nosotros/Mesa_de_trabajo_2.png" alt=""  height="100%">
-            </div> -->
+        <div class="img-mision" style="background-image: url('./imagenes/nosotros/Mesa_de_trabajo_5.png');"  >
+            <!-- <img src="./imagenes/nosotros/Mesa_de_trabajo_5.png" alt="" height="100%" width="100%" style="z-index: -2; position: absolute;"> -->
+            <div class="row">
+                <div class="img-cracks offset-2 col-sm-4">
+                    <img src="./imagenes/nosotros/Mesa_de_trabajo_2.png" alt="" class="img-cracks d-none d-sm-none d-lg-block" height="100%">
+                <div class="tarjeta-mision col-11 col-sm-11" >
+                        <h1 class="sub-title">MISIÓN</h1>
+                        <h4 class="text-2">DE NUESTRA EMPRESA</h4>
+                        <p class="text-1">
+                            Future Solutions Development SAS - FSD SAS
+                            desarrolla e implementta solucionesy modelos 
+                            tecnologicos de TIC, mediante el empleo de Software 
+                            y Hardware. Brindando conectividad mediante 
+                            tecnologia de punta en telecomunicaciones, 
+                            empresarial de I+D+i contribuyendo al bienestar 
+                            de su zona de influencia con la ejecución de 
+                            proyectos basados de tecnologia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
 
      
