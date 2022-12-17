af962c5360d0a6ac17b435b5a74831ed91dcfce4
programaion-ottis <programacion.ottis@gmail.com>
2022-12-17 12:32:34 -0500

    Terminado

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ require("templates/header.html");
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="exampleModalLabel">Clientes Encontrados</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <div class="container" id="tabla_respuesta">
@@ -67,7 +67,7 @@ require("templates/header.html");
                                 </div>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button> -->
                             </div>
                         </div>
                     </div>
