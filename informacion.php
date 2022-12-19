a22925bca6de172609b982f27483b9467c9cfefa
devnet-ottis <ing.devnet.ottis@gmail.com>
2022-12-19 10:55:32 -0500

    modificacion informacion

--- informacion.php
+++ informacion.php
@@ -7,39 +7,39 @@ require("templates/header.html");
         <?php
             require("templates/navbar.html");
         ?>
-        <div class="white_box">
-            
+        <div class="">
+            <img src="./imagenes/normativa/prueba.png" alt="" width="100%" class="fluid-img"> 
+            <div class="">
+                <h2 class="title_regulatory">MARCO REGULATORIO</h2>
+            </div>
         </div>
-        <div class="title_regulatory">
-            <h2>MARCO REGULATORIO</h2>
-        </div>
-
+        <hr style="height: 1px; width: 100%">
         <div class="normative">
             <h3 class="title_nor offset-1"> <strong>Normatividad.</strong></h3>
             <div class="container">
                 <div class="row">
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white protection btn-animate">POLÍTICA <br><strong> PROTECCIÓN <br> DE DATOS </strong></a>
+                        <a href="archivos/Normatividad/001-GRE-POL-Politica de datos personales.pdf" new target="_blank" class="btn btn-white protection btn-animate">POLÍTICA <br><strong> PROTECCIÓN <br> DE DATOS </strong></a>
                     </div>
 
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white treatment btn-animate">AUTORIZACIÓN <br><strong> TRATAMIENTO <br> DE DATOS </strong></a>
+                        <a href="archivos/Normatividad/Anexo_2_Contrato_postpago_OTTIS.pdf" new target="_blank" class="btn btn-white treatment btn-animate">AUTORIZACIÓN <br><strong> TRATAMIENTO <br> DE DATOS </strong></a>
                     </div>
                     
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white treatment2 btn-animate">SOLICITUD <br><strong> TRATAMIENTO <br> DE DATOS </strong></a>
+                        <a href="archivos/Normatividad/049-GRE-FRM-Solicitud Sobre Tratamiento De Datos Personales.pdf" new target="_blank" class="btn btn-white treatment2 btn-animate">SOLICITUD <br><strong> TRATAMIENTO <br> DE DATOS </strong></a>
                     </div>
 
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white contract btn-animate">CONTRATO <br><strong> PRESTACIÓN <br> DEL SERVICIO </strong></a>
+                        <a href="archivos/Normatividad/CONTRATO_UNICO_SERVICIOS_FIJOS.pdf" new target="_blank" class="btn btn-white contract btn-animate">CONTRATO <br><strong> PRESTACIÓN <br> DEL SERVICIO </strong></a>
                     </div>
 
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white billing btn-animate">POLÍTICA <br><strong> DE <br> FACTURACIÓN </strong></a>  
+                        <a href="archivos/Normatividad/CircularFacturacion.pdf" new target="_blank" class="btn btn-white billing btn-animate">POLÍTICA <br><strong> DE <br> FACTURACIÓN </strong></a>  
                     </div>
                     
                     <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                        <a href="#" class="btn btn-white pqr btn-animate">PROCEDIMIENTO <br><strong> PQR </strong></a>
+                        <a href="archivos/Normatividad/ProcedimientoPQR.pdf" new target="_blank" class="btn btn-white pqr btn-animate">PROCEDIMIENTO <br><strong> PQR </strong></a>
                     </div>
                 </div>
             </div>
@@ -53,58 +53,44 @@ require("templates/header.html");
             <div class="container">
                     <div class="row">
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white protection btn-animate">LEY <br><strong> 679/2001 </strong></a>
+                            <a href="archivos/Normatividad/Ley-679-de-2001.pdf" new target="_blank" class="btn btn-white ley btn-animate">LEY <br><strong> 679/2001 </strong></a>
                         </div>
 
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white treatment btn-animate">DECRETO <br><strong> 1524/2002 </strong></a>
+                            <a href="archivos/Normatividad/Decreto 1524 de 2002.pdf" new target="_blank" class="btn btn-white ley btn-animate">DECRETO <br><strong> 1524/2002 </strong></a>
                         </div>
                         
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white treatment2 btn-animate">LEY <br><strong> 1341/2009 </strong></a>
+                            <a href="archivos/Normatividad/Ley 1341 de 2009.pdf" new target="_blank" class="btn btn-white ley btn-animate">LEY <br><strong> 1341/2009 </strong></a>
                         </div>
 
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white contract btn-animate"> DECRETO <br><strong> 4948/2009 </strong></a>
+                            <a href="archivos/Normatividad/Decreto 4948 de 2009.pdf" new target="_blank" class="btn btn-white ley btn-animate"> DECRETO <br><strong> 4948/2009 </strong></a>
                         </div>
 
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white billing btn-animate">POLÍTICA <br><strong> 1078/2015 </strong></a>  
+                            <a href="archivos/Normatividad/Decreto 1078 de 2015.pdf" new target="_blank" class="btn btn-white ley btn-animate">POLÍTICA <br><strong> 1078/2015 </strong></a>  
                         </div>
                         
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white pqr btn-animate">CONTROL <br><strong> PARENTAL </strong></a>
+                            <a href="archivos/Normatividad/ControlParental.pdf" new target="_blank" class="btn btn-white control btn-animate">CONTROL <br><strong> PARENTAL </strong></a>
                         </div>
 
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white pqr btn-animate">SEGURIDAD <br><strong> EN LA WEB</strong></a>
+                            <a href="archivos/Normatividad/seguridad.pdf" new target="_blank" class="btn btn-white protection btn-animate">SEGURIDAD <br><strong> EN LA WEB</strong></a>
                         </div>
 
                         <div class="ok col-12 col-sm-12 col-md-6 col-lg-2">
-                            <a href="#" class="btn btn-white pqr btn-animate">P <br><strong> PQR </strong></a>
+                            <a href="archivos/Normatividad/" new target="_blank" class="btn btn-white explotacion btn-animate">OTTIS CONTRA LA<br><strong>EXPLOTACIÓN <br> INFANTIL</BR> </strong></a>
                         </div>
 
                     </div>
                 </div>
+
+                <hr style="height: 1px; width: 100%">
+
             </div>
-
-
-        
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     </div>
 
